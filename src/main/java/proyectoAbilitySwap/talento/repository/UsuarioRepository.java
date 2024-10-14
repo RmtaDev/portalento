31199bfe90c31638afd81d37f8d9f24d65adbcff
RmtaDev <mticonipa.ra@gmail.com>
2024-10-14 13:50:06 +0200

    por completar logs

--- src/main/java/proyectoAbilitySwap/talento/repository/UsuarioRepository.java
+++ src/main/java/proyectoAbilitySwap/talento/repository/UsuarioRepository.java
@@ -7,11 +7,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.apache.log4j.Logger;
+
 import proyectoAbilitySwap.talento.beans.Usuario;
 
 public class UsuarioRepository {
 	public static final String EXISTE_USUARIO = "SELECT * FROM abilityswapbd.usuarios WHERE usuario = ? AND password = ?";
-	public static final String INSERTAR_USUARIO = "INSERT INTO `abilityswapbd`.`usuarios` (`usuario`, `password`, `foto`) VALUES (?,?,?);";
+	public static final String INSERTAR_USUARIO1= "INSERT INTO `abilityswapbd`.`usuarios` (`usuario`, `password`, `foto`) VALUES (?,?,?);";
+	public static final String INSERTAR_USUARIO = "INSERT INTO `abilityswapbd`.`usuarios` (`usuario`, `password`, `nombre`, `apellidos`, `edad`, `genero`, `telefono`, `email`, `foto`, `rutaFoto`, `hablaSobreTi`) VALUES (?,?,?,?,?,?,?,?,?,?,?);";
+	
+	private static Logger log = Logger.getLogger("mylog");
+
 
 
 	public Usuario leerUsuario(Usuario usuario) throws SQLException {
@@ -29,6 +35,7 @@ public class UsuarioRepository {
 			String nombreu = rs.getString("usuario");
 			String pwdu = rs.getString("password");
 			String rutafoto = rs.getString("rutafoto");
+			
 
 			usuarioLeido = new Usuario(idusuario, nombreu, pwdu, rutafoto);
 		}
@@ -38,26 +45,42 @@ public class UsuarioRepository {
 		return usuarioLeido;
 	}
 /**
+ * Inserta un nuevo usuario en la bse de datos.
  * 
- * @param usuario
- * @return
- * @throws SQLException
+ * @param usuario El objeto Usuario que contiene los datos a insertar en la base de datos.
+ * @return El ID del nuevo usuario insertado en la base de datos, o -1 si no se insertó. 
+ * @throws SQLException Si ocurre algún error de acceso a la base de datos.
  */
 	public int insertarUsuario(Usuario usuario) throws SQLException {
 		int idnuevo = -1;
+		
 		Connection connection = Pool.getConnection();
 		PreparedStatement ps = connection.prepareStatement(INSERTAR_USUARIO, Statement.RETURN_GENERATED_KEYS);
 
 		ps.setString(1, usuario.getUsuario());
 		ps.setString(2, usuario.getPassword());
-		ps.setBinaryStream(3, new ByteArrayInputStream(usuario.getFoto()), usuario.getFoto().length);
+		ps.setString(3, usuario.getNombre());
+		ps.setString(4, usuario.getApellidos());
+		ps.setLong(5, usuario.getEdad());
+		ps.setString(6, usuario.getGenero());
+		ps.setString(7, usuario.getTelefono());
+		ps.setString(8, usuario.getEmail());
+		ps.setBinaryStream(9, new ByteArrayInputStream(usuario.getFoto()), usuario.getFoto().length);
+		ps.setString(10, usuario.getRutaFoto());
+		ps.setString(11, usuario.getHablaSobreTi());
+		
+		log.debug("### -> Ejecutando la consulta de inserción para el usuario: " + usuario.getUsuario());
+
+		
 		//TODO set el resto de parámetros y MODIFICAR la consulta
 		int nfilas = ps.executeUpdate();
+		
 		if (nfilas == 1) {
 			ResultSet rs = ps.getGeneratedKeys();
 			if (rs.next())
 			{
 				idnuevo = rs.getInt(1);
+				log.info("");
 			}
 		}
 

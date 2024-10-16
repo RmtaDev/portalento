de22aaa592229e802ca458f552c18affadfec08c
Dani <danielgp2003@gmail.com>
2024-10-16 12:26:55 +0200

    a

--- src/main/java/proyectoAbilitySwap/talento/beans/HabilidadDemandada.java
+++ src/main/java/proyectoAbilitySwap/talento/beans/HabilidadDemandada.java
@@ -2,4 +2,62 @@ package proyectoAbilitySwap.talento.beans;
 
 public class HabilidadDemandada {
 
+	 private int id_habilidad;
+	    private int usuario;
+	    private String nombre;
+	    private int id_categoria;
+
+	    public HabilidadDemandada() {
+	    }
+
+	    public HabilidadDemandada(int idHabilidad, int usuario, String nombre, int idCategoria) {
+	        this.id_habilidad = idHabilidad;
+	        this.usuario = usuario;
+	        this.nombre = nombre;
+	        this.id_categoria = idCategoria;
+	    }
+
+	    public int getIdHabilidad() {
+	        return id_habilidad;
+	    }
+
+	    public void setIdHabilidad(int idHabilidad) {
+	        this.id_habilidad = idHabilidad;
+	    }
+
+	    public int getUsuario() {
+	        return usuario;
+	    }
+
+	    public void setUsuario(int usuario) {
+	        this.usuario = usuario;
+	    }
+
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public int getIdCategoria() {
+	        return id_categoria;
+	    }
+
+	    public void setIdCategoria(int idCategoria) {
+	        this.id_categoria = idCategoria;
+	    }
+
+
+
+	    @Override
+	    public String toString() {
+	        return "HabilidadOfertada{" +
+	                "idHabilidad=" + id_habilidad +
+	                ", usuario='" + usuario + '\'' +
+	                ", nombre='" + nombre + '\'' +
+	                ", idCategoria=" + id_categoria +
+	                '}';
+	    }
 }

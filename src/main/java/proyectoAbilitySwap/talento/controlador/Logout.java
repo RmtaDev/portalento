6b00a7fe36a813e48800b78be571e2be80e4b971
Vale <valexx55@gmail.com>
2024-10-22 12:04:09 +0200

    logout

---
+++ src/main/java/proyectoAbilitySwap/talento/controlador/Logout.java
@@ -0,0 +1,55 @@
+package proyectoAbilitySwap.talento.controlador;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Servlet implementation class Logout
+ * servlet para cerrar la sesión
+ */
+
+public class Logout extends HttpServlet {
+	
+	private static Logger log = Logger.getLogger("mylog");
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.debug("En Logout");
+		HttpSession session =  request.getSession(false);
+		if (session!=null)
+		{
+			log.debug("El usuario tiene sesión " +session.getId());
+			session.invalidate();
+			log.debug("Sesión invalidada");
+			//le mandamos a login
+			request.getRequestDispatcher("login.html").forward(request, response);
+		} else {
+			log.debug("El usuario NO tiene sesión. Le dirigimos a inicio ");
+			//la mandamos a incio
+			//request.getRequestDispatcher("login.html").forward(request, response);
+			response.sendRedirect("inicio.html");
+		}
+		
+	}
+
+
+}

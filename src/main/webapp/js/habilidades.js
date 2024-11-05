675d2b739e2bb352c6c34640176e10472cce0244
RmtaDev <mticonipa.ra@gmail.com>
2024-11-05 01:14:05 +0100

    por terminar insertar Habilidad

---
+++ src/main/webapp/js/habilidades.js
@@ -0,0 +1,58 @@
+// funcion para insertar un habilidad ofertada
+function insertarHabilidadOfertada() {
+	const boton = document.getElementById("botonInsertar");
+
+	boton.addEventListener("click", function() {
+		console.log("haz tocado el boton de insertar");
+		const idCategoria = document.getElementById("ofertadas").value;
+		const habilidadOfertada = document.getElementById("texto").value;
+
+
+		if (idCategoria && habilidadOfertada) {
+			console.log(`Has insertado : Categoria = ${idCategoria}, Texto = ${habilidadOfertada}`);
+
+			//Crear la URL con parametros de consulta
+			const url = `http://localhost:8080/proyectoAbilitySwap/InsertarHabilidadesOfertadas?habilidadOfertada=${encodeURIComponent(habilidadOfertada)}&idCategoria=${encodeURIComponent(idCategoria)}`;
+
+			// Enviar los datos al servidor usando fetch
+			fetch(url, {
+				method: "POST",
+				
+			})
+				.then(response => {
+					if (response.ok) {
+						return response.json();
+					} else {
+						throw new Error("Error en la respuesta del servidor");
+					}
+
+				})
+				.then(data => {
+					console.log("respuesta del servidor: ", data);
+					//alert("Habilidad insertada exitosamente");
+					
+					const container = document.getElementById("container-hb-ofertadas");
+					const nuevoElemento = document.createElement("div");
+					
+					nuevoElemento.className = "etiqueta";
+					nuevoElemento.textContent = habilidadOfertada;
+					container.appendChild(nuevoElemento);
+				})
+				.catch(error => {
+					console.error("Hubo un problema con la solicitud: ", error);
+					alert("Error al insertar la hibilidad. Intentelo de nuevo.");
+				})
+
+		}
+
+		else {
+			console.log("por favor, completa todos los campos.");
+			alert("Por favor, completa todos los campos antes de insertar.");
+		}
+
+
+	});
+
+}
+
+document.addEventListener("DOMContentLoaded", insertarHabilidadOfertada);
